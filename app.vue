e06a7fd44981fd979fd03877d7d4fee21046a385
Goran Savic <goran.savic@keepersolutions.com>
2022-06-02 13:38:54 +0200

    init commit

---
+++ app.vue
@@ -0,0 +1,29 @@
+<template>
+
+    <Header/>
+    <div class="container">
+      <h1>Welcome to Restaurantly</h1>
+      <a href="/restaurants">Go to restaurants</a>
+    </div>
+
+</template>
+
+<style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  max-width: 1440px;
+  margin: 0 auto;
+  border: 1px solid #eee;
+}
+
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,46 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+<header class="header">
+  <h3 class="logo">Restaurantly</h3>
+  <ul class="navigation">
+    <li>
+      <a href="/">Home</a>
+    </li>
+  <li>
+      <a href="/restaurants">Restaurants</a>
+    </li>
+  </ul>
+</header>
+
+</template>
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  margin: 0;
+  padding: 10px;
+  box-shadow: 0 1px 3px #808080;
+  background-color: coral;
+  .logo {
+    color: #eee;
+  }
+  .navigation {
+    display: flex;
+    align-items: center;
+    list-style-type: none;
+    li {
+      padding: 15px;
+      a {
+        text-decoration: none;
+        color: #eee;
+      }
+    }
+  }
+}
+
+</style>
